eeb427755d1d1b37bc5541ca099d2877fd4ec5ea
Rashmi Sharma <rashmi.sharma@mavq.com>
2023-12-04 00:07:09 +0530

    file changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import User from './pages/User';
-import CreateUser from './pages/CreateUser';
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import User from "./pages/User";
+import CreateUser from "./pages/CreateUser";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Hero />} />
-          <Route path='/users' exact element={<User />} />
-          <Route path='/create-users' exact element={<CreateUser/>} />
+          <Route path="/" exact element={<Hero />} />
+          <Route path="/users" exact element={<User />} />
+          <Route path="/create-users" exact element={<CreateUser />} />
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
