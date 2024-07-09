1470707f5ca7027739b28176cdce53e1dfc8d9dc
KILLERTIAN <omsharma050322@gmail.com>
2024-07-09 12:26:09 +0530

    vercel fail

--- api/server.js
+++ api/server.js
@@ -2,36 +2,54 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import userRoutes from "./routes/user.route.js";
 
-const app = express();
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
 mongoose.set("strictQuery", true);
 
+// Connect to MongoDB
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB");
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
   }
 };
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/users", userRoutes);
+
+// Serve static files from the React app
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong!";
-  
-  // Sending error response
   res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8000, () => {
+// Start the server
+app.listen(PORT, () => {
   connect();
-  console.log("listening on port 8000");
+  console.log(`Server running on port ${PORT}`);
 });
